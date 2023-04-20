30ffcce0cf8b0557caf993ee4b4c57c6f02afae9
SebasVargas2 <savargaspaez@gmail.com>
2023-04-20 08:50:40 -0500

    2Version lista

--- main.js
+++ main.js
@@ -4,43 +4,60 @@ const operando = document.getElementById('operando')
 const display = document.getElementById('display')
 const limpiar = document.getElementById('limpiar')
 const botones = document.querySelector('.calculadora')
+let numero1= 0
 
 botones.addEventListener('click',(event)=>{
     if (event.target.id=='suma'){
         operando.textContent="+"
+        numero1+=num1.value
+        num1.value=""
     }
     if (event.target.id=='resta'){
         operando.textContent="-"
+        numero1+=num1.value
+        num1.value=""
     }
     if (event.target.id=='mult'){
         operando.textContent="x"
+        numero1+=num1.value
+        num1.value=""
     }
     if (event.target.id=='divi'){
         operando.textContent="/"
+        numero1+=num1.value
+        num1.value=""
     }
     if (event.target.id=='limpiar'){
         operando.textContent=""
         display.textContent=""
         num1.value=""
-        num2.value=""
+        numero1=0
     }
 
     if (event.target.id=='igual'){
         if (operando.textContent=="+"){
-            let suma=+num1.value+ (+num2.value)
+            let suma=+num1.value+ (+numero1)
             display.textContent=suma
+            num1.value=""
+            numero1=0
         }
         else if (operando.textContent=="-"){
-            let resta=+num1.value- (+num2.value)
+            let resta= +numero1-(+num1.value)
             display.textContent=resta
+            num1.value=""
+            numero1=0
         }
         else if (operando.textContent=="x"){
-            let mul=+num1.value * (+num2.value)
+            let mul= +numero1*(+num1.value)
             display.textContent=mul
+            num1.value=""
+            numero1=0
         }
         else if (operando.textContent=="/"){
-            let div=+num1.value / (+num2.value)
+            let div= +numero1/(+num1.value)
             display.textContent=div
+            num1.value=""
+            numero1=0
         }
         else {
             alert('Digite los numero o la operacion a realizar')
